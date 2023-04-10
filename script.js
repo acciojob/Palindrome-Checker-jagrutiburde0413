5859b4533a5622704b983ced5c3a1df1ec5f46d5
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-10 23:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // complete the given function
 
 function palindrome(str){
-    let flag=false;
-	str=str.toLowerCase()
-	for(let i=0;i<Math/floor(str.length/2);i++){
-		if(str[i]!==str[str.length-i-1]){
-			flag=true;
-			break;
-		}
-	}
-	return !flag
-}
+    str=str.toLowerCase();
+    let i=0;
+    let j=str.length-1;
+    while(i<j){
+        if(str.charAt(i)==str.charAt(j)){
+            i++;
+            j--;
+        }else{
+           return false;
+        }
+    }
+    return true;
+    
+    }
 module.exports = palindrome
